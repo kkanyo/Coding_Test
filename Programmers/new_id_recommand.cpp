9e45448d0c827033972e665428b62de0ff1c1a99
kkanyo <tjrhks12@gmail.com>
2021-11-14 19:15:33 +0900

    2021 KAKAO BLIND ECRUITMENT 신규 아이디 추천 (최적화 필요)

---
+++ Programmers/new_id_recommand.cpp
@@ -0,0 +1,94 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void Level1(string& str)
+{
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (isupper(str[i]))
+            str[i] = tolower(str[i]);
+    }
+}
+
+void Level2(string& str)
+{
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (islower(str[i]) || isdigit(str[i]) ||
+           str[i] == '-' || str[i] == '_' || str[i] == '.') {
+            
+        }
+        else {
+            str.erase(i--, 1);
+        }
+    }
+}
+
+void Level3(string& str)
+{
+    for (int i = 1; i < str.size(); i++)
+    {
+        if (str[i] == '.')
+        {
+            if (str[i - 1] == '.')
+                str.erase(i--, 1);
+        }
+    }
+}
+
+void Level4(string& str)
+{
+    if (str[0] == '.')
+        str.erase(0, 1);
+    
+    int last = str.size() - 1;
+    
+    if (str[last] == '.')
+        str.erase(last, 1);
+}
+
+void Level5(string& str)
+{
+    if(str.empty())
+        str += 'a';
+}
+
+void Level6(string& str)
+{
+    int size = str.size();
+    
+    if (size >= 16) {
+        str.erase(15, size - 15);
+        
+        if (str[14] == '.')
+            str.erase(14, 1);
+    }
+    
+}
+
+void Level7(string& str)
+{
+    int size = str.size();
+    
+    if (size <= 2) {
+        str.append(3 - size, str[size - 1]);
+    }
+}
+
+string solution(string new_id) {
+    string answer = "";
+    
+    answer = new_id;
+    
+    Level1(answer);
+    Level2(answer);
+    Level3(answer);
+    Level4(answer);
+    Level5(answer);
+    Level6(answer);
+    Level7(answer);
+    
+    return answer;
+}
