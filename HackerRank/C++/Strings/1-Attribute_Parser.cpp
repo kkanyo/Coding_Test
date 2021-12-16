316721ba73c23e2e3c017b9cfa94fde065405687
kkanyo <kkanyo33@gmail.com>
2021-12-16 19:35:27 +0900

    incorrect values in node with problem about to pointer

--- HackerRank/C++/Strings/1-Attribute_Parser.cpp
+++ HackerRank/C++/Strings/1-Attribute_Parser.cpp
@@ -6,139 +6,132 @@
 using namespace std;
 
 /*
-This time Let's use tree structure...
-It's sooooo hard problem for me :(
-but do not give up
+String values are incorrectly input to node...
+I think this problem is about to 'pointer'
+If it doesn't work even after trying agiain, refer to answer
 */
-struct hrml {
+
+struct tree_node {
     string tag;
     vector<string> attributes;
     vector<string> values;
-};
-
-struct tree_node {
-    struct hrml node;
     vector<struct tree_node*> children;
     struct tree_node* parent;
 };
 
+void HrmlParse(string hrml, struct tree_node* current_level) {
+    int i = 1;
+    
+    //for closing tag
+    if (hrml[i] == '/') {
+        current_level = current_level->parent;
+    }
+    else {
+        struct tree_node* new_node = new struct tree_node;
+        //input tag
+        while (hrml[i] != ' ' && hrml[i] != '>') i++;
+        new_node->tag = hrml.substr(1, i-1);
+        
+        int mark;
+        
+        while (hrml[i] != '>') {
+            mark = ++i;
+            
+            //input attributes
+            while (hrml[i] != ' ') i++;
+            new_node->attributes.push_back(hrml.substr(mark, i-mark));
+            
+            //input value
+            while (hrml[i] != '\"') i++;
+            mark = ++i;
+            while (hrml[i] != '\"') i++;
+            new_node->values.push_back(hrml.substr(mark, i-mark));
+            ++i;
+        }
+        
+        current_level->children.push_back(new_node);
+        new_node->parent = current_level;
+        current_level = new_node;
+    }
+}
+
 int main() {
     int num_of_lines, num_of_queries;
     struct tree_node* root = new struct tree_node;
-    struct tree_node* now_level = root;
-    
-    string strinput;
+    struct tree_node* current_level = root;
     
     scanf("%d %d\n", &num_of_lines, &num_of_queries);
 
-    //input and parse HRML 
+    //input and parse HRML
     for (int i = 0; i < num_of_lines; i++) {
-        string strtmp = "";
+        string hrml;
         
-        getline(cin, strinput);
-        cin.ignore();
+        getline(cin, hrml);
         
-        int strinput_size = strinput.size();
+        HrmlParse(hrml, current_level);
+    }
+    
+    //query input
+    for (int i = 0; i < num_of_queries; i++) {  
+        string query;
+        int j = 0;
+        int mark = 0;
+              
+        getline(cin, query);
         
-        //parsing
-        for (int j = 1; j < strinput_size; j++) {
-            //for end tag
-            if (strinput[j] == '/')
-            {
-                j += 1;
-                while (j < strinput_size - 1) {
-                    strtmp += strinput[j];
-                }
-                now_level->node.tag = strtmp;
-                now_level = now_level->parent;
-            }
-            else {
-                struct tree_node* new_node = new struct tree_node;
+        //first tag compare
+        while(query[j] != '.' && query[j] != '~') j++;
+        if (current_level->tag != query.substr(mark, j)) {
+            cout << current_level->tag;
+            
+            printf("Not Found!1\n");
+            continue;
+        }
+        
+        mark = j++;
+        
+        //another tags compare
+        while(query[j] != '~') {
+            j++;
+            
+            if(query[j] != '.') {
+                int num_children = current_level->children.size();
+                int k = 0;
+                bool correctTag = false;
                 
-                if (strinput[j] == ' ') {
-                    new_node->node.attributes.push_back(strtmp);
-                    strtmp = "";
+                for (; k < num_children; k++) {
+                    if (current_level->children.at(k)->tag == query.substr(mark, j-mark)) {
+                        current_level = current_level->children.at(k);
+                        correctTag = true;
+                        break;
+                    }
                 }
-                else if (strinput[j] == '=') {
-                    j += 2;
-                }
-                else if (strinput[j] == '\"') {
-                    new_node->node.attributes.push_back(strtmp);
-                    strtmp = "";
-                }
-                else if (strinput[j] == '>') {
+                
+                if (!correctTag) {
+                    printf("Not Found!2\n");
                     break;
                 }
-                else {
-                    strtmp += strinput[j];
-                }
                 
-                now_level->children.push_back(new_node);
-                now_level = new_node;
+                mark = j + 1;
             }
+        }
+        
+        //compare attribute
+        int num_attribute = current_level->attributes.size();
+        bool correctAtr = false;
+        
+        for (int k = 0; k < num_attribute; k++) {
+            if (query.substr(mark, query.size()-mark) == current_level->attributes.at(k)) {
+                cout << current_level->values.at(k) << endl;
+                correctAtr = true;
+                break;
+            }
+        }
+        
+        if (!correctAtr) {
+            printf("Not Found!3\n");
         }
     }
     
-    /*
-    //query input
-    for (int i = 0; i < num_of_queries; i++) {
-        string strtmp = "";
-        vector<string> query;
-        
-        getline(cin, strinput);
-        
-        int strinput_size = strinput.size();
-        
-        //parse query
-        for (int j = 0; j < strinput_size; j++) {
-            if (strinput[j] == '.') {
-                query.push_back(strtmp);
-                strtmp = "";
-            }
-            else if (strinput[j] == '~') {
-                query.push_back(strtmp);
-                strtmp = "";
-                
-                while (j < strinput_size - 1) {
-                    strtmp += strinput[++j];
-                }
-                
-                query.push_back(strtmp);
-            }
-            else {
-                strtmp += strinput[j];
-            }
-        }
-        
-        int t = tag.size() - 1;
-        
-        //compare tag
-        for (int j = 0; j < query.size() - 2; j++) {
-            for (; t > 0; t--) {
-                if (query[j] != tag[t]) {
-                    printf("Not Found!\n");
-                    break;
-                }
-            }
-        }
-        
-        int select_hrml;
-
-        //matching tag to hrml
-        for (int j = 0; j < num_of_lines/2; j++) {
-            if (hrml[j][0] == tag[t]) {
-                for (int k = 1; k < hrml[j].size() - 1; k += 1) {
-                    if (hrml[j][k].compare(query[query.size() - 1]) == 0) {
-                        cout << hrml[j][k + 1] << endl;
-                        break;
-                    }
-                    else {
-                        printf("Not Found!\n");
-                    }
-                }
-            }
-        }
-    }
-    */
     return 0;
 }
