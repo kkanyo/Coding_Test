38ffb2b7ff2d82b4206377671a44dd189e3ee11c
kkanyo <kkanyo33@gmail.com>
2022-01-10 16:43:44 +0900

    feat(Participant who did not complete): Solve 'Participant who did not complete' problem of hash on Programmers
    Using hashmap

---
+++ Programmers/Hash/1-Participant_who_didnt_complete_Hashmap.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    map<string, int> mapParticipant;                //string: participant name
+                                                    //int: the number of non-complete participant (nums)
+    
+    for (auto i : participant) {
+        if (mapParticipant.count(i) > 0) {          //If paticipants who have same name,
+            mapParticipant.find(i)->second += 1;    //increase 'nums'
+        }
+        mapParticipant.insert(make_pair(i, 1));
+    }
+    
+    for (auto j : completion) {                 
+        auto item = mapParticipant.find(j);         //If participant complete the marathon,
+        item->second -= 1;                          //decrease 'nums'
+    }
+    
+    for (auto k : mapParticipant) {                 //If nums == 0, that participant complete the marathon.
+        if (k.second == 1) {                        //If nums >= 1, he/she did not complete it.
+            answer = k.first;
+        }
+    }
+    
+    return answer;
+}
