6a954bf010368a34ef00aacb2a2ed086586adff2
kkanyo <kkanyo33@gmail.com>
2022-01-10 21:43:05 +0900

    feat(Hotel Prices): Solve the 'Hotel Prices' problem of c++ on HackerRank
    'room' is declared to point type of HotelRoom.
    Although 'room' is instance of HotelApartment, get_price() of parents class is executed because of dynamic binding.
    So, the correct function is called for an object by change HotelRoom::get_price() to virtual function.

---
+++ HackerRank/C++/Debugging/1-Hotel_Prices.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class HotelRoom {
+public:
+    HotelRoom(int bedrooms, int bathrooms) 
+    : bedrooms_(bedrooms), bathrooms_(bathrooms) {}
+    
+    virtual int get_price() {                   //cause: Add 'virtual' keyword
+        return 50*bedrooms_ + 100*bathrooms_;
+    }
+private:
+    int bedrooms_;
+    int bathrooms_;
+};
+
+class HotelApartment : public HotelRoom {
+public:
+    HotelApartment(int bedrooms, int bathrooms) 
+    : HotelRoom(bedrooms, bathrooms) {}
+
+    int get_price() {
+        return HotelRoom::get_price() + 100;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    vector<HotelRoom*> rooms;       //declared to point type of HotelRoom
+    for (int i = 0; i < n; ++i) {
+        string room_type;
+        int bedrooms;
+        int bathrooms;
+        cin >> room_type >> bedrooms >> bathrooms;
+        if (room_type == "standard") {
+            rooms.push_back(new HotelRoom(bedrooms, bathrooms));
+        } else {
+            rooms.push_back(new HotelApartment(bedrooms, bathrooms));
+        }
+    }
+
+    int total_profit = 0;
+    for (auto room : rooms) {               //Although 'room' is instance of HotelApartment,
+        total_profit += room->get_price();  //get_price() of parents class is executed
+    }                                       //because of dynamic binding.
+    cout << total_profit << endl;
+
+    for (auto room : rooms) {
+        delete room;
+    }
+    rooms.clear();
+
+    return 0;
+}
