d832547cbbb87cf3fa5facb47fbcf0a8aa2535f5
kkanyo <kkanyo33@gmail.com>
2022-01-30 16:11:59 +0900

    docs(Apple and Orange): add comment about code

--- HackerRank/Algorithms/Implementation/2-Apple_and_Orange.cpp
+++ HackerRank/Algorithms/Implementation/2-Apple_and_Orange.cpp
@@ -21,11 +21,15 @@ vector<string> split(const string &);
 void countApplesAndOranges(int s, int t, int a, int b, vector<int> apples, vector<int> oranges) {
     int numApples = 0, numOranges = 0;
     
+    // Check position of apples
     for (auto& it : apples) {
+        // Calculate position of fruit
         int posApple = it+a;
-        if (posApple >= s && posApple <= t) { numApples++; }
+        // If the fruit located in house
+        if (posApple >= s && posApple <= t) { numApples++; }    // Increase # of fruits
     }
     
+    // Check position of oranges
     for (auto& it : oranges) {
         int posOrange = it+b;
         if (posOrange >= s && posOrange <=t) { numOranges++; }
