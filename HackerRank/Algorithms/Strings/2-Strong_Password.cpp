b098a13cc40efb295663c0039fe2538cf0c6de5c
kkanyo <kkanyo33@gmail.com>
2022-01-30 17:24:24 +0900

    refactor(Strong Password): Simplified the code that checks to contain special characters

--- HackerRank/Algorithms/Strings/2-Strong_Password.cpp
+++ HackerRank/Algorithms/Strings/2-Strong_Password.cpp
@@ -6,31 +6,35 @@
  *  1. INTEGER n
  *  2. STRING password
  */
-
+ 
 int minimumNumber(int n, string password) {
     // Return the minimum number of characters to make the password strong
     int result = 0;
+    
+    string special_ch = "!@#$%^&*()-+";
     bool check[4] = {false,}; // 0:number, 1:lower case, 2:upper case, 3:special character
     
     for (auto& ch : password) {
-        if (ch >= '0' && ch <= '9') {           // check containing number
+        if (ch >= '0' && ch <= '9') {           // check to contain number
             check[0] = true; 
-        } else if (ch >= 'a' && ch <= 'z') {    // check containing lower case
+        } else if (ch >= 'a' && ch <= 'z') {    // check to contain lower case
             check[1] = true; 
-        } else if (ch >= 'A' && ch <= 'Z') {    // check containing upper case
+        } else if (ch >= 'A' && ch <= 'Z') {    // check to contain upper case
             check[2] = true; 
-        } else if (ch=='!' || ch=='@' || ch=='#' || ch=='$' || ch =='%'
-            || ch=='^' || ch=='&' || ch=='*' || ch=='(' || ch ==')'
-            || ch=='-' || ch=='+') {            // check containing special character
-                check[3] = true;
+        } else {
+            for (auto& sp_ch : special_ch) {    // check to contain special character
+                if (ch == sp_ch) { check[3] = true; }
             }
+        }
     }
     
-    for (auto& it : check) {    // If password do not contain character for strong password
+    // If password do not contain character for strong password
+    for (auto& it : check) {    
         if (!it) result++;
     }
     
-    if (n + result < 6) {       // If password with character requiring is shorter than 6
+    // If password with character requiring is shorter than 6
+    if (n + result < 6) {       
         result += 6-(n+result);
     }
     
