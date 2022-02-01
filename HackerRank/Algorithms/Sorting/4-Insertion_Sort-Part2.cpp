100bb7a0e1947ab4a190040a0a7e2fd2072f7b76
kkanyo <kkanyo33@gmail.com>
2022-02-01 18:51:02 +0900

    feat(Insertion Sort-Part2): Solve 'Insertion Sort-Part2' problem about 'Sorting'

---
+++ HackerRank/Algorithms/Sorting/4-Insertion_Sort-Part2.cpp
@@ -0,0 +1,29 @@
+/*
+ * Complete the 'insertionSort2' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY arr
+ */
+ 
+// Print elements of vector
+void printArr(vector<int> arr) {
+    for (auto &num : arr) {
+        cout << num << " ";
+    }
+    cout << "\n";
+}
+
+void insertionSort2(int n, vector<int> arr) {
+    for (int i = 1; i < n; i++) {
+        int tmp = arr[i];
+        int j = i-1;    // prev
+        
+        while (arr[j] > tmp) {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = tmp;
+        printArr(arr);
+    }
+}
