7d2327d66342d68b3d6590088c37f8f13d1c41cf
kkanyo <kkanyo33@gmail.com>
2022-02-03 21:45:05 +0900

    feat(Sherlock and Array): Solve 'Sherlock and Array' problem about 'Search'

---
+++ HackerRank/Algorithms/Search/3-Sherlock_and_Array.cpp
@@ -0,0 +1,28 @@
+/*
+ * Complete the 'balancedSums' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+string balancedSums(vector<int> arr) {
+    int size = arr.size();
+    // If size of array is 1, answer is always "YES"
+    if (size == 1) return "YES";
+    
+    int left_sum = 0;   // Sum of element of left subarray
+    int right_sum = 0;  // Sum of element of right subarray
+    
+    for (auto &num : arr) {
+        right_sum += arr[i];
+    }
+    
+    // Update 'left_sum' and 'right_sum' based on current element
+    for (auto &num : arr) {
+        right_sum -= num;
+        if (left_sum == right_sum) { return "YES"; }
+        left_sum += num;
+    }
+    
+    return "NO";
+}
