7f373da08e52de7047ae954638e15436c468f288
kkanyo <kkanyo33@gmail.com>
2022-04-02 18:48:23 +0900

    refacot(Number_Line_Jumps): not use loop

--- HackerRank/Algorithms/Implementation/3-Number_Line_Jumps.cpp
+++ HackerRank/Algorithms/Implementation/3-Number_Line_Jumps.cpp
@@ -9,16 +9,32 @@
  *  4. INTEGER v2
  */
 
+// Use loop
 string kangaroo(int x1, int v1, int x2, int v2) {
     // If jump degree of first kangaroo is shorter than second one
     // it cannot follow sceond one
-    if (v1 <= v2) return "NO";
+    int _x1 = x1;   int _x2 = x2;
+    int _v1 = v1;   int _v2 = v2;
+
+    if (_v1 <= _v2) return "NO";
     
-    while (x1 < x2) {
-        x1 += v1;
-        x2 += v2;
+    while (_x1 < _x2) {
+        _x1 += _v1;
+        _x2 += _v2;
     }
     
-    if (x1 == x2) { return "YES"; }
+    if (_x1 == _x2) { return "YES"; }
     else return "NO";
+}
+
+// O(1)
+// Not use loop
+string kangaroo(int x1, int v1, int x2, int v2) {
+    if (v1 <= v2) return "NO";
+    
+    int n_distance = x2 - x1;
+    int n_speed = v1 - v2;
+    
+    // Remainder is not 0 means that kangaroo1 jump over kangaroo2
+    return (n_distance % n_speed == 0) ? "YES" : "NO"; 
 }
