d1c6da1616724101c240571ec679b222fc446961
kkanyo <kkanyo33@gmail.com>
2022-07-31 12:41:19 +0900

    feat: 신고 결과 받기 풀이
    - https://school.programmers.co.kr/learn/courses/30/lessons/92334
     + 구조체와 map 자료구조를 이용하여 데이터 관리
     + stringstream을 이용하여 문자열 파싱
    - 코드 정리 및 최적화 작업 필요

---
+++ Programmers/Level1/신고_결과_받기.cpp
@@ -0,0 +1,75 @@
+#include <string>
+#include <vector>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+struct ReportInfo {
+    string sUserID;
+    vector<string> slReportID;
+};
+vector<ReportInfo> lReportInfo;
+vector<string> slBlockID;
+
+// id_list  유저 ID
+// report   유저가 신고한 ID
+// k        게시판 이용 정지 기준
+vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+    vector<int> answer;
+    unordered_map<string, int> mpCountReport;
+    
+    // init
+    for ( const auto &id : id_list ) {
+        ReportInfo tempReport;
+        tempReport.sUserID = id;
+        lReportInfo.push_back(tempReport);
+        mpCountReport.insert( { id, 0 } );
+    }
+    
+    // 신고 목록 중복 제거
+    set<string> slDdeduplicateReport;
+    for ( const auto &userReport : report ) {
+        slDdeduplicateReport.insert(userReport);
+    }
+    
+    // 신고 내용 파싱
+    for ( const auto &userReport : slDdeduplicateReport) {
+        stringstream ss(userReport);
+        string sUserID;
+        ss >> sUserID;
+        for ( auto &reportInfo : lReportInfo ) {
+            if ( sUserID == reportInfo.sUserID ) {
+                string sReportID;
+                ss >> sReportID;
+                reportInfo.slReportID.push_back( sReportID );
+                
+                mpCountReport[sReportID]++;
+                
+                break;
+            }
+        }
+    }
+    
+    // 정지 유저 선별
+    for ( const auto &countReport : mpCountReport ) {
+        if ( countReport.second >= k ) {
+            slBlockID.push_back( countReport.first );
+        }
+    }
+    
+    // 메일 발송 카운트
+    for ( auto &reportInfo : lReportInfo ) {
+        int nMailCount = 0;
+        for ( const auto &blockID : slBlockID) {
+            if ( find( reportInfo.slReportID.begin(), reportInfo.slReportID.end(), blockID ) != reportInfo.slReportID.end() ) {
+                nMailCount++;
+            }
+        }
+        answer.push_back(nMailCount);
+    }
+    
+    return answer;
+}
