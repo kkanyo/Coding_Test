862a14285003001007c74a79372927991ff41b97
kkanyo <kkanyo33@gmail.com>
2022-09-07 15:45:12 +0900

    feat: Solve the "Breaking the Records"

---
+++ HackerRank/Algorithms/Implementation/4-Braking_the_Recoreds.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+
+/*
+ * Complete the 'breakingRecords' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY scores as parameter.
+ */
+
+int* breakingRecords(std::vector<int> scores) {
+    int nvTimesBreakRec[2] = {0};
+    
+    int nMaxScore = scores[0];
+    int nMinScore = nMaxScore;
+    
+    for (const auto& elem : scores)
+    {
+        if (elem > nMaxScore)
+        {
+            nMaxScore = elem; 
+            nvTimesBreakRec[0]++;
+        }
+        else if (elem < nMinScore)
+        {
+            nMinScore = elem;
+            nvTimesBreakRec[1]++;
+        }
+    }
+    
+    return nvTimesBreakRec;
+}
