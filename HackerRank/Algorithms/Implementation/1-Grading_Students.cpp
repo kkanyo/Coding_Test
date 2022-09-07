a019d0dd848745d624d27e8a260af78d537aaec3
kkanyo <kkanyo33@gmail.com>
2022-09-07 15:27:20 +0900

    refactor: initialze vector with explicit size and rename var

--- HackerRank/Algorithms/Implementation/1-Grading_Students.cpp
+++ HackerRank/Algorithms/Implementation/1-Grading_Students.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+
 /*
  * Complete the 'gradingStudents' function below.
  *
@@ -5,16 +7,31 @@
  * The function accepts INTEGER_ARRAY grades as parameter.
  */
 
-vector<int> gradingStudents(vector<int> grades) {
-    vector<int> result;
-    
-    for(auto& grade : grades) {
-        int remainder = grade%5;
-        if (grade >= 35 && remainder >= 3) {    // If grade is upper to 35
-            grade += (5-remainder);             // Round to Nearest 5
+#define DIVISION_STAND 5
+#define FAIL_STAND 38
+
+std::vector<int> gradingStudents(std::vector<int> nvOriginalGrades) {    
+    int nIndexGrades = 0;
+    int nNumStudents = nvOriginalGrades.size();
+    std::vector<int> nvFinalGrades(nNumStudents);
+
+    for (const auto &elem : nvOriginalGrades)
+    {
+        if ( elem < FAIL_STAND) 
+        {
+            nvFinalGrades[nIndexGrades] = elem;
         }
-        result.push_back(grade);
+        else
+        {
+            int nRemainder = elem % DIVISION_STAND;
+        
+            if ( nRemainder < 3)
+                nvFinalGrades[nIndexGrades] = elem;
+            else
+                nvFinalGrades[nIndexGrades] = elem + (DIVISION_STAND - nRemainder);
+        }
+        nIndexGrades++;
     }
     
-    return result;
+    return nvFinalGrades;
 }
