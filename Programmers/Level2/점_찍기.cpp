4335909e1114fc9002b680cbd2f2757b881cd8dd
kkanyo <kkanyo33@gmail.com>
2022-12-04 15:21:51 +0900

    feat: programmers '점 찍기' 문제 풀이
    정수 연산만을 이용한 풀이

---
+++ Programmers/Level2/점_찍기.cpp
@@ -0,0 +1,23 @@
+// 정수 연산만을 이용한 방법
+long long solution1(int k, int d) {
+    long long answer = 0;
+    int posY = d - d % k;
+    
+    for (int posX = 0; posX <= d; posX += k)
+    {
+        // 찍히는 점은 호를 그리므로, 이전 y값보다 클 수 없다
+        for (; posY >= 0; posY -= k)
+        {
+            if ( (long long)posX * posX + (long long)posY * posY
+                    <= (long long)d * d )
+            {
+                // 최초로 찍은 점의 y값을 이용하여 세로축 갯수 추산
+                // y = 0 에 대해서도 고려하는 것 주의
+                answer += posY / k + 1;
+                break;
+            }
+        }
+    }
+
+    return answer;
+}
